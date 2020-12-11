1c9eec3d8ff82cb2cce778f3dce4f0fce1aa7862
Nate Walck <github@natewalck.com>
2020-12-11 17:05:24 -0500

    Remove hacks and use requirements.txt to solve the problem (#11)
    * Fix cffi install call
    
    * A more simple fix that seems to work well
    
    * Also remove wheel argument, missed that one
    
    Co-authored-by: Nathan Walck <nw@squareup.com>

--- locallibs/install.py
+++ locallibs/install.py
@@ -73,18 +73,13 @@ def install_requirements(requirements_file, framework_path, version):
     if not os.path.exists(python_path):
         print("No python at %s" % python_path, file=sys.stderr)
         return
-    if version.startswith("3.9"):
-        # nasty hack to get xattr to install under 3.9.1rc1
-        with open(requirements_file) as rfile:
-            if "xattr" in rfile.read():
-                install("cffi", framework_path, version)
     cmd = [python_path, "-s", "-m", "pip", "install", "-r", requirements_file]
     print("Installing modules from %s..." % requirements_file)
     subprocess.check_call(cmd)
 
 
 def install_extras(framework_path, version="2.7", requirements_file=None,
-                   install_wheel=False, upgrade_pip=False):
+                   upgrade_pip=False):
     """install all extra pkgs into Python framework path"""
     print()
     python_guard_path = os.path.expanduser(
@@ -104,8 +99,6 @@ def install_extras(framework_path, version="2.7", requirements_file=None,
     ensure_pip(framework_path, version)
     if upgrade_pip:
         upgrade_pip_install(framework_path, version)
-    if install_wheel:
-        install("wheel", framework_path, version)
     if requirements_file:
         install_requirements(requirements_file, framework_path, version)
     elif version.startswith("2."):
